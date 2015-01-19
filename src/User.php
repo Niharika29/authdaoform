73e23cb8863acd204091dfeb5a091301ab011120
Niharika Kohli <niharikakohli29@gmail.com>
2015-01-19 23:55:44 +0530

    Key ingredients

---
+++ src/User.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * @section LICENSE
+ * This file is part of Wikimania Scholarship Application.
+ *
+ * Wikimania Scholarship Application is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Wikimania Scholarship Application is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Wikimania Scholarship Application.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @file
+ * @copyright © 2013 Bryan Davis and Wikimedia Foundation.
+ * @copyright © 2013 Calvin W. F. Siu, Wikimania 2013 Hong Kong organizing team
+ * @copyright © 2012-2013 Katie Filbert, Wikimania 2012 Washington DC organizing team
+ * @copyright © 2011 Harel Cain, Wikimania 2011 Haifa organizing team
+ * @copyright © 2010 Wikimania 2010 Gdansk organizing team
+ * @copyright © 2009 Wikimania 2009 Buenos Aires organizing team
+ */
+
+namespace authdaoform;
+
+use \PDOException;
+use Password;
+
+/**
+ * Data access object for scholarship applications.
+ */
+class User extends AbstractDao {
+
+	public function getUser( $username ) {
+		return $this->fetch(
+			'SELECT * FROM users WHERE username = ? AND isvalid = 1',
+			array( $username )
+		);
+	}
+
+	public function getUsername( $id ) {
+		return $this->fetch(
+			'SELECT username FROM users WHERE id = ?',
+			array( $id )
+		);
+	}
+
+	public function getListofUsers( $state ) {
+		return $this->fetchAll( 'SELECT * FROM users' );
+	}
+
+	public function getUserInfo( $user_id ) {
+		return $this->fetch(
+			"SELECT * FROM users WHERE id = ?",
+			array( $user_id )
+		);
+	}
+
+	public function isSysAdmin( $user_id ) {
+		$res = $this->fetch(
+			"SELECT isadmin FROM users WHERE id = ?",
+			array( $user_id )
+		);
+		return $res['isadmin'];
+	}
+
+	public function newUserCreate( $answers ) {
+		$fields = array(
+			'username', 'password', 'email', 'reviewer', 'isvalid', 'isadmin'
+		);
+		$placeholders = array();
+		$vals = array();
+		foreach ( $fields as $field ) {
+			$placeholders[] = ":{$field}";
+			$vals[$field] = $answers[$field];
+		}
+
+		$sql = 'INSERT INTO users (' .
+			implode( ', ', $fields ) . ') VALUES (' .
+			implode( ',', $placeholders ) . ')';
+
+		return $this->insert( $sql, $vals );
+	}
+
+	/**
+	 * @param array $answers Updated user data
+	 * @param int $id User id
+	 * @return bool True if update suceeded, false otherwise
+	 */
+	public function updateUserInfo( $answers, $id ) {
+		$fields = array(
+			'username', 'email', 'reviewer', 'isvalid', 'isadmin', 'blocked'
+		);
+		$placeholders = array();
+		foreach ( $fields as $field ) {
+			$placeholders[] = "{$field} = :{$field}";
+		}
+
+		$sql = self::concat(
+			'UPDATE users SET',
+			implode( ', ', $placeholders ),
+			'WHERE id = :id'
+		);
+		$stmt = $this->dbh->prepare( $sql );
+		$answers['id'] = $id;
+
+		try {
+			$this->dbh->beginTransaction();
+			$stmt->execute( $answers );
+			$this->dbh->commit();
+			return true;
+
+		} catch ( PDOException $e) {
+			$this->dbh->rollback();
+			$this->logger->error( 'Failed to update user', array(
+				'method' => __METHOD__,
+				'exception' => $e,
+				'sql' => $sql,
+				'params' => $answers,
+			) );
+			return false;
+		}
+	}
+
+	public function updatePassword( $oldpw, $newpw, $id, $force = null ) {
+		if ( !$force ) {
+			$res = $this->fetch(
+				'SELECT password FROM users WHERE id = ?',
+				array( $id )
+			);
+
+			if ( !Password::comparePasswordToHash( $oldpw, $res['password'] ) ) {
+				// passsword doesn't match expected
+				$this->logger->notice( 'Invalid old password; will not update', array(
+					'method' => __METHOD__,
+					'user' => $id,
+				) );
+				return false;
+			}
+		}
+
+		$stmt = $this->dbh->prepare( 'UPDATE users SET password = ? WHERE id = ?' );
+		try {
+			$this->dbh->beginTransaction();
+			$stmt->execute( array( Password::encodePassword( $newpw ), $id ) );
+			$this->dbh->commit();
+			$this->logger->notice( 'Changed password for user', array(
+					'method' => __METHOD__,
+					'user' => $id,
+			) );
+			return true;
+
+		} catch ( PDOException $e) {
+			$this->dbh->rollback();
+			$this->logger->error( 'Failed to update password for user', array(
+				'method' => __METHOD__,
+				'exception' => $e,
+			) );
+			return false;
+		}
+	}
+
+	public function userIsBlocked( $id ) {
+		$res = $this->query( "SELECT blocked FROM users WHERE id = ?", array( $id ) );
+		return $res['blocked'];
+	}
+
+}
